5e6a9d4f5144bd213dfcb794c62693c33a87a1cf
U-WBI\nxa17016 <nxa17016@NXL95523.wbi.nxp.com>
2018-04-27 17:49:01 +0530

    Model training wrapper function

--- code/TrainUtils.py
+++ code/TrainUtils.py
@@ -9,6 +9,9 @@ import numpy as np
 import keras.backend as K
 from itertools import product
 from keras.utils import to_categorical
+from keras.callbacks import ModelCheckpoint
+from keras.optimizers import SGD
+from keras.losses import binary_crossentropy
 
 def w_categorical_crossentropy(y_true, y_pred, weights):
     nb_cl = len(weights)
@@ -27,3 +30,25 @@ def batch_temporal_categorical(y,n_classes):
         oh = [to_categorical(x,n_classes) for x in y[i]]
         yoh[i]=oh;
     return yoh;
+
+def train_model(model,save_path,trgen,valgen,
+                epochs=1,verbose=1,ctc_mode=False,
+                loss=binary_crossentropy,
+                optimizer=SGD(lr=0.02, decay=1e-6, momentum=0.9, 
+                    nesterov=True, clipnorm=5),
+                steps_per_epoch=100, validation_steps=10,
+                metrics = ['acc']):
+
+    if ctc_mode:
+        raise ValueError("CTC based model training is not yet implemented")
+
+    checkpointer = ModelCheckpoint(filepath=save_path, verbose=1)
+    model.compile(optimizer=optimizer,loss=loss,metrics=metrics);
+    hist = model.fit_generator(generator=trgen,
+                               validation_data=valgen,
+                               epochs=epochs,
+                               steps_per_epoch=steps_per_epoch,
+                               validation_steps=validation_steps,                           
+                               callbacks=[checkpointer],
+                               verbose=verbose);
+    return hist
