75e6afd7085605805cf95a42f5eba774cee5508a
U-WBI\nxa17016 <nxa17016@NXL95523.wbi.nxp.com>
2018-04-27 16:34:28 +0530

    Added colab package installer

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#------------------------------------------------------------------------------
+# Date : Apr 27 2018
+# Madhur Kashyap 2016EEZ8350
+# Necessary packages for running code on google colaboratory
+#------------------------------------------------------------------------------
+pip install -U -q PyDrive
+pip install librosa
+pip install sphfile
+pip install python_speech_features
