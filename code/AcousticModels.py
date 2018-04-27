337054353373d69d491751472d9eaf54977c8c97
U-WBI\nxa17016 <nxa17016@NXL95523.wbi.nxp.com>
2018-04-27 17:08:33 +0530

    Optionally support CuDNN models

--- code/AcousticModels.py
+++ code/AcousticModels.py
@@ -10,27 +10,28 @@ from keras.models import Model, Sequential
 from keras.layers import (BatchNormalization, Conv1D, Dense, Input, 
     TimeDistributed, Activation, Bidirectional, SimpleRNN, GRU, LSTM,
     CuDNNGRU, Dropout, Flatten)
-from keras.optimizers import SGD
 
-
-def bidi_gru(input_dim, units, output_dim):
+def bidi_gru(input_dim, units, output_dim,gpu=False):
     model = Sequential();
-    model.add(Bidirectional(GRU(units, return_sequences=True,
+    if gpu:
+        model.add(Bidirectional(CuDNNGRU(units, return_sequences=True,
                                 batch_input_shape=(None,None,input_dim))))
-    model.add(Dense(output_dim,activation='softmax'))
-    #model.output_length = lambda x: x
+    else:
+        model.add(Bidirectional(GRU(units, return_sequences=True,
+                                batch_input_shape=(None,None,input_dim))))
+
+    model.add(TimeDistributed(Dense(output_dim,activation='softmax')))
     print(model.summary())
     return model
 
-    
-def gru(input_dim, units, output_dim):
+def uni_gru(input_dim, units, output_dim, gpu=False):
     model = Sequential();
-    model.add(GRU(units, return_sequences=True,
-                                batch_input_shape=(None,None,input_dim)))
-
-    # https://github.com/keras-team/keras/issues/3009
-    #model.add(Flatten());
+    if gpu:
+        model.add(CuDNNGRU(units, return_sequences=True,
+                           batch_input_shape=(None,None,input_dim)))
+    else:
+        model.add(GRU(units, return_sequences=True,
+                           batch_input_shape=(None,None,input_dim)))
     model.add(TimeDistributed(Dense(output_dim,activation='softmax')))
-    model.output_length = lambda x: x
     print(model.summary())
     return model
