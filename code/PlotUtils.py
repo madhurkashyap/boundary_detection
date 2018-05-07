b903f9e358349a64e57d968aa23155456c9ba4f9
U-WBI\nxa17016 <nxa17016@NXL95523.wbi.nxp.com>
2018-05-07 21:31:11 +0530

    history plotter supports only loss case

--- code/PlotUtils.py
+++ code/PlotUtils.py
@@ -33,29 +33,38 @@ def plot_keras_history(history,suptitle='',figsize=(8,4),legendloc='lower center
     Accepts history dictionary object as input. Plots both training
     and validation accuracy and loss curves against epochs
     '''
-    x = list(range(len(history['acc'])));
-    f, (ax1, ax2) = plt.subplots(nrows=1,ncols=2,figsize=figsize)
-    f.suptitle(suptitle)
+
+    x = list(range(len(history['loss'])));
+    # Check if has accuracy as well
+    if 'acc' in history:
+        f, (ax1, ax2) = plt.subplots(nrows=1,ncols=2,figsize=figsize)
+        f.suptitle(suptitle)
+        ax2.set_xlabel('# Epoch')
+        ax2.set_ylabel('Accuracy')
+        y1b=history['acc'];
+        line2, = ax2.plot(x,y1b,'b',label="Train")
+        line4, = ax2.plot(x,y2b,'r',label="Test")
+        if 'val_acc' in history.keys():
+            y2b=history['val_acc'];
+        else:
+            legpt = [line1]; legtxt = ['Train'];
+        axes = [ax1,ax2]
+    else:
+        f, ax1 = plt.subplots(nrows=1,ncols=1,figsize=figsize)
+        axes = [ax1];
+
     ax1.set_xlabel('# Epoch')
     ax1.set_ylabel('Loss')
-    ax2.set_xlabel('# Epoch')
-    ax2.set_ylabel('Accuracy')
-
     y1a=history['loss'];
-    y1b=history['acc'];
     line1, = ax1.plot(x,y1a,'b',label="Train")
-    line2, = ax2.plot(x,y1b,'b',label="Train")
-    if 'val_acc' in history.keys():
+
+    if 'val_loss' in history:
         y2a=history['val_loss'];
-        y2b=history['val_acc'];
         line3, = ax1.plot(x,y2a,'r',label="Test")
-        line4, = ax2.plot(x,y2b,'r',label="Test")
         legpt = (line1,line3); legtxt = ('Train','Test');
-    else:
-        legpt = [line1]; legtxt = ['Train'];
-    f.legend(legpt,legtxt,legendloc,
+        f.legend(legpt,legtxt,legendloc,
                  fontsize='small',ncol=2,frameon=False)
-    for ax in [ax1,ax2]:
+    for ax in axes:
         for item in ([ax.title, ax.xaxis.label, ax.yaxis.label] +
              ax.get_xticklabels() + ax.get_yticklabels()):
             item.set_fontsize(9)
