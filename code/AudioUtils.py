a92a5c46a62b98500b02f7c6c4d65df22f96c930
U-WBI\nxa17016 <nxa17016@NXL95523.wbi.nxp.com>
2018-05-08 12:25:15 +0530

    extract_features support logfb, window control

--- code/AudioUtils.py
+++ code/AudioUtils.py
@@ -9,20 +9,22 @@ import os
 import librosa
 import numpy as np
 from sphfile import SPHFile
-from python_speech_features import mfcc
+from python_speech_features import mfcc,logfbank
 
 def read_sph(path):
     assert os.path.exists(path), "SPH file not readable"
     sph = SPHFile(path);
     return sph.format['sample_rate'], sph.content,
 
-def extract_mfcc_features(y,sr,n_mfcc=13,roc=False,roa=False):
+def extract_features(y,sr,n_mfcc=13,wlen=0.025,wstep=0.01,
+                     roc=False,roa=False,logfb=False):
     """
     fmax has been default to 8k allowing 4khz audio frequency samples
     """
-    m = mfcc(y,samplerate=sr,numcep=n_mfcc);
+    m = mfcc(y,samplerate=sr,winlen=wlen,winstep=wstep,numcep=n_mfcc)
     if roc or roa: d = librosa.feature.delta(m);
     if roa: dd = librosa.feature.delta(d);
     if roc: m = np.hstack((m,d));
     if roa: m = np.hstack((m,dd));
+    if logfb: m = np.hstack((m,logfbank(y,sr,wlen,wstep)));
     return m
